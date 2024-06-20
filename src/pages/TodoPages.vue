f769a70dc03057128e363ba68d2aa34ffbcb2827
Daniel Kopecký <kopecky.d@gmail.com>
2024-06-20 10:54:26 +0200

    9. Added TodoPage

---
+++ src/pages/TodoPages.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page padding>
+    <AddTodo />
+    <TodoList />
+  </q-page>
+</template>
+
+<script setup>
+import AddTodo from 'components/AddTodo.vue';
+import TodoList from 'components/TodoList.vue';
+</script>
