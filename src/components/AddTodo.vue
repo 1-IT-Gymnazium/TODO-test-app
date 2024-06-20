53aa3f689614f6b5455c98f92c75a0c607f3de01
Daniel Kopecký <kopecky.d@gmail.com>
2024-06-20 10:43:13 +0200

    4. Component AddTodo

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-card>
+    <q-card-section>
+      <q-input v-model="title" placeholder="New Todo" />
+    </q-card-section>
+    <q-card-actions>
+      <q-btn label="Add" @click="addTodo" />
+    </q-card-actions>
+  </q-card>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import {useTodoStore} from "stores/todo-store";
+
+
+const title = ref('');
+const todoStore = useTodoStore();
+
+const addTodo = () => {
+  if (title.value) {
+    todoStore.addTodo({ title: title.value, completed: false });
+    title.value = '';
+  }
+};
+</script>
