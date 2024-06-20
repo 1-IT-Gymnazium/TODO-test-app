35caa7447376f409529fc40d3603b09e56a7cdc9
Daniel Kopecký <kopecky.d@gmail.com>
2024-06-20 10:52:52 +0200

    7. Component TodoList

---
+++ src/components/TodoList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div v-for="todo in todos" :key="todo.id">
+      <TodoItem :todo="todo" @update="updateTodo" @delete="deleteTodo" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+import { onMounted } from 'vue';
+import TodoItem from 'components/TodoItem.vue';
+import {useTodoStore} from "stores/todo-store";
+
+const todoStore = useTodoStore();
+const todos = todoStore.$state.todos;
+
+onMounted(() => {
+  todoStore.fetchTodos();
+});
+
+const updateTodo = async (updatedTodo) => {
+  await todoStore.updateTodo(updatedTodo);
+};
+
+const deleteTodo = (id) => {
+  todoStore.removeTodo(id);
+};
+</script>
